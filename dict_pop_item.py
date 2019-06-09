015551c2e7067d3dde0d81efbbe759513a53a176
NEHAISRANI <nehai18@navgurukul.org>
2019-06-09 11:52:05 +0530

    Returns the last item of the given dictionary by returning (key, value)

---
+++ dict_pop_item.py
@@ -0,0 +1,14 @@
+Dictionary1 = {'A': 'Geeks', 'B': 'For', 'C': 'Geeks'} 
+print Dictionary1
+Dictionary1['D']=Dictionary1.pop('A')
+print Dictionary1
+
+
+# name={"name1":"rakesh","name2":"neha","name3":"pragati","name4":"priya"}
+# print (name)
+# name["name5"]=name.pop("name1")
+# print(name)
+
+person = {'name': 'Phill', 'age': 22, 'salary': 3500.0}
+result = person.popitem()
+print result
